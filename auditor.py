f09e5ec3b7f988bd0f5d2c2c2ba4ab8db66f4468
Si Liang <2605328@sit.singaporetech.edu.sg>
2026-09-11 15:00:27 +0800

    Added auditor program code

--- auditor.py
+++ auditor.py
@@ -1,3 +1,26 @@
-a = 2
-b = 3
-print(a + b)
+import sys
+
+inventory = 0
+rejectedEntryCount = 0
+
+user_input = input("Enter the stock quantity in inventory: ")
+
+while user_input != "quit":
+    if user_input.isdigit() == False or int(user_input) < 0: 
+        rejectedEntryCount += 1
+        print("Error! Please enter a valid integer.")
+        user_input = input("Enter the stock quantity in inventory: ")
+    elif user_input=="quit":
+        sys.exit()
+    else:
+        inventory += int(user_input)
+
+        if inventory < 500:
+            print(f"Current inventory: {inventory}")
+            user_input = input("Enter the stock quantity in inventory: ")
+        elif inventory > 500:
+            print("Inventory count exceeds 500.")
+            print(f"Total Units Processed: {inventory}")
+            print(f"Total Rejected Entries: {rejectedEntryCount}")
+            break;
+    
